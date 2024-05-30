566345c9f654494047ffcc9442c7fbca48c196d1
kabir-125 <kabir.ahmed@enosisbd.com>
2024-05-30 15:45:05 +0600

    test working

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,14 @@
+import Link from "next/link";
+
 export default function Home(){
     
     return(
-        <div>Dashboard</div>
+        <>
+            <h1>Dashboard</h1>
+            <Link href='/'>
+                < div className="text-lg text-gray-700 hover:text-gray-900 underline">Go back to Home </div>
+            </Link>
+        </>
+        
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,13 @@ export default function Dashboard() {
   return (
     <>
        <div className="text-xl font-bold text-blue-600 mb-4">Home</div>
-      <Link href='/dashboard'>
-        < div className="text-lg text-gray-700 hover:text-gray-900 underline">Go to dashboard </div>
-      </Link>
+       <div>
+        Log in to view
+        <Link href='/dashboard'>
+          < div className="text-lg text-gray-700 hover:text-gray-900 underline"> Dashboard </div>
+        </Link>
+       </div>
+      
     </>
     
   );
